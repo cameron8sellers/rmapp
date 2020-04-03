9e11e905d30943622305596b2e7a379e4e12526f
Cameron Sellers <cameronsmac@Camerons-MacBook-Pro.local>
2020-04-03 12:21:01 -0600

    Add more content to profile

--- src/components/CharacterDeats.js
+++ src/components/CharacterDeats.js
@@ -8,6 +8,10 @@ function CharacterDetails(props) {
       <img src={props.char[0].image} alt={props.char[0].name} />
       <h3>Name: {props.char[0].name}</h3>
       <p>Species: {props.char[0].species}</p>
+      <p>Location: {props.char[0].location.name}</p>
+      <p>Origin: {props.char[0].origin.name}</p>
+      <p>Status: {props.char[0].status} </p>
+      <p>Joined the List: {props.char[0].created} </p>
     </div>
   );
 }
